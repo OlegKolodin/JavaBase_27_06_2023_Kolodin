5d87d57b726fc5d97c285f0b5df37d15abfa9d2c
Oleh Kolodin <Oleg_kp87@ukr.net>
2023-07-09 21:44:29 +0200

    HelloWorld.java has been changed

--- src/main/java/ua/hillel/kolodin/homeworks/homework1/HelloWorld.java
+++ src/main/java/ua/hillel/kolodin/homeworks/homework1/HelloWorld.java
@@ -4,5 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("hello Java");// for checking commits at repositoriy
+        System.out.println(3344);
+        int num = 3344;
+        System.out.println();
     }
 }

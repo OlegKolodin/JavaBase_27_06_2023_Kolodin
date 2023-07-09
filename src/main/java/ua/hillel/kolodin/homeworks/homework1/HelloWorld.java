55ceeba582c341b8906d42a01e34f89be0800c97
Oleh Kolodin <Oleg_kp87@ukr.net>
2023-07-09 21:10:38 +0200

    changed java file in Homework1, checking how is working Git

--- src/main/java/ua/hillel/kolodin/homeworks/homework1/HelloWorld.java
+++ src/main/java/ua/hillel/kolodin/homeworks/homework1/HelloWorld.java
@@ -3,5 +3,6 @@ package ua.hillel.kolodin.homeworks.homework1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("hello Java");// for checking commits at repositoriy
     }
 }
